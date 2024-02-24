58e168a5bef383deed3c7635c5bc849f9e9986f9
sanket <sanketkopnar1299@gmail.com>
2024-02-24 22:50:47 +0530

    User model is created

---
+++ Backend/src/models/user.models.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+const UserSchema = new Schema(
+   {
+      fullname: {
+         type: String,
+         required: true,
+      },
+      email: {
+         type: String,
+         required: true,
+         unique: true,
+      },
+      password: {
+         type: String,
+         required: true,
+         minlength: 6,
+      },
+   },
+   { timestamps: true }
+);
+
+UserSchema.methods.generateAcessToken = async function () {
+   return jwt.sign(
+      {
+         _id: this._id,
+         email: this.email,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+         expiresIn: process.env.ACESS_TOKEN_EXPIRY,
+      }
+   );
+};
+
+UserSchema.methods.generateRefreshToken = async function () {
+   return jwt.sign(
+      {
+         _id: this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+         expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      }
+   );
+};
+
+export const User = mongoose.model("user", UserSchema);
